6f747aa93746d6dea1c0ea0d2967f4fa142221ec
electr0strike <136839175+ajaykavuri@users.noreply.github.com>
2026-03-24 22:11:11 +0000

    added boilerplate for consumer

---
+++ tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
@@ -0,0 +1,5 @@
+from consumer_interface import mqConsumerInterface
+
+class mqConsumer(mqConsumerInterface):
+    def __init__(self, binding_key: str, exchange_name: str, queue_name: str):
+        pass
