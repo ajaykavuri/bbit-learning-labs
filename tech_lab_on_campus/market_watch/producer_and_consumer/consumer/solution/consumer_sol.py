23f18e02e26e8a95aaef3788e16bcf56c7e45eba
electr0strike <136839175+ajaykavuri@users.noreply.github.com>
2026-03-24 23:13:30 +0000

    finished producer/consumer tasks

--- tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
+++ tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
@@ -1,23 +1,23 @@
 import pika
 import os
+from consumer_interface import mqConsumerInterface
 
 class mqConsumer(mqConsumerInterface):
     def __init__(self, binding_key: str, exchange_name: str, queue_name: str):
         self.binding_key = binding_key
         self.exchange_name = exchange_name
         self.queue_name = queue_name
-        self.setupRMQConnection()
         self.channel = None
         self.exchange = None
         self.connection = None
-
+        self.setupRMQConnection()
     def setupRMQConnection(self) -> None:
-        if not self.channel:
-            self.channel = self.connection.channel()
-            self.channel.queue_declare(queue={self.queue_name})
-        if not self.exchange:
-            self.exchange = self.channel.exchange_declare(exchange="Exchange Name")
-            
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=con_params)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name)
+        self.exchange = self.channel.exchange_declare(exchange="Exchange Name")
+
         self.channel.queue_bind(
                 queue= self.queue_name,
                 routing_key= self.binding_key,
@@ -25,16 +25,16 @@ class mqConsumer(mqConsumerInterface):
             )
         
         self.channel.basic_consume(
-            self.queue_name, self.on_message_callback(), auto_ack=False
+            self.queue_name, self.on_message_callback, auto_ack=False
         )
 
-     def on_message_callback(
+    def on_message_callback(
         self, channel, method_frame, header_frame, body
     ) -> None:
         
         channel.basic_ack(method_frame.delivery_tag, False)
         
-        print(header_frame + " " + body)
+        print(body)
 
         
     def startConsuming(self) -> None:
